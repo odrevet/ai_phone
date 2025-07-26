047990ce773ed546fa248d8dec3e58531e063ada
odrevet <odrevet@gmail.com>
2025-07-26 08:23:24 +0200

    sms view layout

--- lib/widgets/sms_view.dart
+++ lib/widgets/sms_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_bubble/chat_bubble.dart';
 
 import '../api.dart';
 
@@ -17,99 +16,259 @@ class SMSView extends StatefulWidget {
   SMSViewState createState() => SMSViewState();
 }
 
-class SMSViewState extends State<SMSView> {
+class SMSViewState extends State<SMSView> with TickerProviderStateMixin {
   final TextEditingController _messageController = TextEditingController();
   final ScrollController _scrollController = ScrollController();
+  bool _isTyping = false;
+  late AnimationController _animationController;
+  late Animation<int> _dotAnimation;
 
   @override
   void initState() {
     super.initState();
-    // Scroll to bottom initially
     WidgetsBinding.instance.addPostFrameCallback((_) => _scrollToBottom());
+
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+
+    _dotAnimation = IntTween(begin: 0, end: 3).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.linear),
+    );
   }
 
   void _scrollToBottom() {
-    _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+  }
+
+  Widget _buildTypingIndicator() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.05),
+                  blurRadius: 4,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: AnimatedBuilder(
+              animation: _dotAnimation,
+              builder: (context, child) {
+                String dots = '.' * (_dotAnimation.value + 1);
+                return Text(
+                  'Typing$dots',
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                    fontSize: 14,
+                    fontStyle: FontStyle.italic,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessageBubble(Map<String, String> message) {
+    final isUser = message['role'] == 'user';
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: isUser
+            ? MainAxisAlignment.start
+            : MainAxisAlignment.end,
+        children: [
+          if (!isUser) const Spacer(flex: 1),
+          Flexible(
+            flex: 4,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(
+                color: isUser
+                    ? Colors.grey.shade100
+                    : Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 4,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Text(
+                message['content'] ?? '',
+                style: TextStyle(
+                  color: isUser ? Colors.black87 : Colors.white,
+                  fontSize: 16,
+                  height: 1.4,
+                ),
+              ),
+            ),
+          ),
+          if (isUser) const Spacer(flex: 1),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEmptyState() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.chat_bubble_outline,
+            size: 64,
+            color: Colors.grey.shade400,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'No messages yet',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.grey.shade600,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'Start a conversation below',
+            style: TextStyle(fontSize: 14, color: Colors.grey.shade500),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInputArea() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(top: BorderSide(color: Colors.grey.shade200, width: 1)),
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade50,
+                    borderRadius: BorderRadius.circular(24),
+                    border: Border.all(color: Colors.grey.shade300, width: 1),
+                  ),
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: const InputDecoration(
+                      hintText: 'Type a message...',
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 12,
+                      ),
+                    ),
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                    enabled: !_isTyping,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 12),
+              Container(
+                decoration: BoxDecoration(
+                  color: _isTyping
+                      ? Colors.grey.shade300
+                      : Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: IconButton(
+                  onPressed: _isTyping
+                      ? null
+                      : () async {
+                          await _sendMessage();
+                        },
+                  icon: Icon(
+                    Icons.send,
+                    color: _isTyping ? Colors.grey.shade500 : Colors.white,
+                  ),
+                  padding: const EdgeInsets.all(12),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   void dispose() {
     _messageController.dispose();
     _scrollController.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('SMS View')),
+      backgroundColor: Colors.grey.shade50,
+      appBar: AppBar(
+        title: const Text('SMS View'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black87,
+        elevation: 0,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1),
+          child: Container(height: 1, color: Colors.grey.shade200),
+        ),
+      ),
       body: Column(
         children: [
           Expanded(
-            child: widget.conversationHistory.isEmpty
-                ? const Center(
-                    child: Text(
-                      'No messages yet',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  )
+            child: widget.conversationHistory.isEmpty && !_isTyping
+                ? _buildEmptyState()
                 : ListView.builder(
                     controller: _scrollController,
-                    itemCount: widget.conversationHistory.length,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    itemCount:
+                        widget.conversationHistory.length + (_isTyping ? 1 : 0),
                     itemBuilder: (context, index) {
-                      final message = widget.conversationHistory[index];
+                      if (_isTyping &&
+                          index == widget.conversationHistory.length) {
+                        return _buildTypingIndicator();
+                      }
 
-                      return ChatBubble(
-                        alignment: message['role'] == 'user'
-                            ? Alignment.topLeft
-                            : Alignment.topRight,
-                        clipper: ChatBubbleClipper1(
-                          type: message['role'] == 'user'
-                              ? BubbleType.receiverBubble
-                              : BubbleType.sendBubble,
-                        ),
-                        backGroundColor: message['role'] == 'user'
-                            ? const Color(0xffE7E7ED)
-                            : Colors.grey,
-                        margin: const EdgeInsets.only(top: 20),
-                        child: Container(
-                          constraints: BoxConstraints(
-                            maxWidth: MediaQuery.of(context).size.width * 0.7,
-                          ),
-                          child: Text(
-                            message['content'] ?? '',
-                            style: const TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      );
+                      final message = widget.conversationHistory[index];
+                      return _buildMessageBubble(message);
                     },
                   ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _messageController,
-                    decoration: const InputDecoration(
-                      hintText: 'Type a message',
-                      border: OutlineInputBorder(),
-                    ),
-                    maxLines: null,
-                    keyboardType: TextInputType.multiline,
-                  ),
-                ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: () async {
-                    await _sendMessage();
-                  },
-                  child: const Text('Send'),
-                )
-              ],
-            ),
-          ),
+          _buildInputArea(),
         ],
       ),
     );
@@ -119,17 +278,33 @@ class SMSViewState extends State<SMSView> {
     final message = _messageController.text.trim();
     if (message.isNotEmpty) {
       await widget.addConversation("user", message);
-      _scrollToBottom();
-
       _messageController.clear();
 
-      sendChatCompletion(widget.conversationHistory, "assistant").then((
-        response,
-      ) async {
-        String messageContent = response['choices'][0]['message']['content'];
-        await widget.addConversation("assistant", messageContent);
-        _scrollToBottom();
+      setState(() {
+        _isTyping = true;
       });
+      _animationController.repeat();
+      _scrollToBottom();
+
+      sendChatCompletion(widget.conversationHistory, "assistant")
+          .then((response) async {
+            _animationController.stop();
+            setState(() {
+              _isTyping = false;
+            });
+
+            String messageContent =
+                response['choices'][0]['message']['content'];
+            await widget.addConversation("assistant", messageContent);
+            _scrollToBottom();
+          })
+          .catchError((error) {
+            _animationController.stop();
+            setState(() {
+              _isTyping = false;
+            });
+            print('Error sending message: $error');
+          });
     }
   }
 }
